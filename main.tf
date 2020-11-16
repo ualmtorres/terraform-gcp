9c6b0ce0f6beb5bf0824478b956f4726855181e2
Manuel Torres Gil <mtorres@ual.es>
2020-11-17 00:15:05 +0100

    Commit inicial

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  version = "3.5.0"
+
+  credentials = file("cc2021-clouddi-891d2046b463.json")
+
+  project = "cc2021-clouddi"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
